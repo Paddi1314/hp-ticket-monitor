19ec41b250df671b2fee450ef2cb0f6c0dba4ceb
Lin Jingxuan <qwe1411782119@163.com>
2026-05-31 12:42:28 +0800

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,23 +1,23 @@
 from playwright.sync_api import sync_playwright
 
 with sync_playwright() as p:
-    browser = p.chromium.launch(headless=True)
+    browser = p.chromium.launch(
+        headless=True
+    )
 
     page = browser.new_page()
 
     page.goto(
-        "https://book.wbstudiotour.com/?event_type_id=2&language_id=1&site_id=1",
-        wait_until="networkidle"
+        "https://book.wbstudiotour.com/?event_type_id=2&language_id=1&site_id=1"
     )
 
+    page.wait_for_timeout(10000)
+
     print("标题：", page.title())
-    print("当前URL：", page.url)
 
-    print("\n等待60秒...\n")
+    print("URL：", page.url)
 
-    page.wait_for_timeout(60000)
-
-    print("60秒后标题：", page.title())
-    print("60秒后URL：", page.url)
+    print("Cookie：")
+    print(page.context.cookies())
 
     browser.close()
