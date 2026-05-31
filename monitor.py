226a4c88b7aa218098520b6c794fdefa73da7d58
Lin Jingxuan <qwe1411782119@163.com>
2026-05-31 12:35:52 +0800

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,4 +1,5 @@
 from playwright.sync_api import sync_playwright
+import json
 
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=True)
@@ -11,7 +12,12 @@ with sync_playwright() as p:
     )
 
     print("标题：", page.title())
-
     print("当前URL：", page.url)
+
+    # 看看页面源码前1000个字符
+    html = page.content()
+
+    print("\n===== 页面源码前1000字符 =====\n")
+    print(html[:1000])
 
     browser.close()
